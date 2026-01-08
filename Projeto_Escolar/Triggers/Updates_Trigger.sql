e4bf794ae8666fddd90710ed61e72da48b6a8e95
luisoliva77 <luisfsoliva@gmail.com>
2026-01-08 17:05:07 -0300

    Create Updates_Trigger.sql

---
+++ Projeto_Escolar/Triggers/Updates_Trigger.sql
@@ -0,0 +1,15 @@
+UPDATE ALUNO SET EMAIL  = 'ricardo.s2@escola.com'
+WHERE IDALUNO = 1
+GO
+
+UPDATE ALUNO SET EMAIL  = 'carlos.dourado@escola.com'
+WHERE IDALUNO = 7
+GO
+
+UPDATE ALUNO SET EMAIL  = 'sara.lima@escola.com'
+WHERE IDALUNO = 11
+GO
+
+UPDATE ALUNO SET EMAIL  = 'simone.matos@escola.com'
+WHERE IDALUNO = 20
+GO
